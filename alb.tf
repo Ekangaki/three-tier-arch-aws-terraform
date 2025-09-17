571c62d44fe8e9195b88a713d7c9a48565faac09
E Njang <georgegedeon2012@gmail.com>
2025-09-17 12:53:23 -0400

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -17,48 +17,32 @@ module "alb_http_sg" {
 # Application load balancer (ALB)
 ################################################################################
 
-module "alb" {
-  source          = "terraform-aws-modules/alb/aws"
-  version         = "~> 8.0"
-  name            = var.alb_name
-  vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.public_subnets
-  security_groups = [module.alb_http_sg.security_group_id]
+resource "aws_lb_target_group" "alb_target_group" {
+  name     = var.alb_target_group_name
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
 
-  # This is the corrected block for listeners
-  listeners = {
-    http_80 = {
-      port     = 80
-      protocol = "HTTP"
-      forward = {
-        target_group_key = "default"
-      }
-    }
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/phpinfo.php"
+    port                = "traffic-port"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 6
+    protocol            = "HTTP"
+    matcher             = "200-399"
   }
+}
 
-  target_groups = {
-    default = {
-      name             = var.alb_target_group_name
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      stickiness = {
-        enabled = true
-        type    = "lb_cookie"
-      }
-      health_check = {
-        enabled             = true
-        interval            = 30
-        path                = "/phpinfo.php"
-        port                = "traffic-port"
-        healthy_threshold   = 3
-        unhealthy_threshold = 3
-        timeout             = 6
-        protocol            = "HTTP"
-        matcher             = "200-399"
-      }
-    }
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = module.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
   }
-
-  tags = var.alb_tags
 }
