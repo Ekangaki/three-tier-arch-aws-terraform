9aefdf0c3d798e497a12f301fdb1edf28fe6d12e
E Njang <georgegedeon2012@gmail.com>
2025-09-17 12:20:44 -0400

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -6,28 +6,29 @@ locals {
   user_data = <<-EOT
 #!/bin/bash
 yum update -y
-amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-yum install -y httpd
+yum install -y httpd mariadb-server php-fpm php-mysqlnd
 systemctl start httpd
 systemctl enable httpd
+systemctl start mariadb
+systemctl enable mariadb
+systemctl start php-fpm
+systemctl enable php-fpm
 usermod -a -G apache ec2-user
 chown -R ec2-user:apache /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
-echo '<?php phpinfo(); ?>' > /var/www/html/phpinfo.php
-sudo yum install php-mbstring php-xml -y
-sudo systemctl restart httpd
-sudo systemctl restart php-fpm
+
+# PHPMyAdmin Installation
 cd /var/www/html
 wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz
 mkdir phpMyAdmin && tar -xvzf phpMyAdmin-latest-all-languages.tar.gz -C phpMyAdmin --strip-components 1
 rm phpMyAdmin-latest-all-languages.tar.gz
-echo '<?php phpinfo(); ?>' > /var/www/html/phpinfo.php
 cd phpMyAdmin
 mv config.sample.inc.php config.inc.php
 sed -i 's/localhost/${module.rds.db_instance_address}/g' config.inc.php
-  EOT
+sed -i "s/\['db_port'\] = ''/\['db_port'\] = 3306/g" config.inc.php
+EOT
 }
 
 ################################################################################
